f5b67c49931e656ec81224b69e247f947fa31967
chrisindris <christopher.indris@gmail.com>
2026-05-10 04:26:59 -0700

    separate experiments for fir

--- vsibench_eval_compute_canada_wrapper.sh
+++ vsibench_eval_compute_canada_wrapper.sh
@@ -2,9 +2,27 @@
 
 # run this script on the login node
 
+# --- RUN 1 ---
+
 _MODEL_LIST=(
-  cvis-tmu/videor1sft-lora-sft-Scene30k_traineval_5epochs_merged
-  cvis-tmu/videor1-lora-sft-Scene30k_traineval_5epochs_merged
+  cvis-tmu/videor1-lora-sft-Scene30k_traineval_426steps_merged
+  cvis-tmu/videor1-lora-sft-Scene30k_traineval_852steps_merged
+)
+_ML=''
+for i in "${_MODEL_LIST[@]}"; do 
+  _ML="${_ML}${i}," 
+done
+
+MODEL_LIST=${_ML%,}  # remove trailing comma
+export MODEL_LIST
+export OVERLAY_IMG="/scratch/indrisch/thinking-in-space/containers/apptainer-overlay.img"
+
+sbatch vsibench_eval_compute_canada.sh
+
+
+# --- RUN 2 ---
+
+_MODEL_LIST=(
   cvis-tmu/Qwen2.5-VL-7B-COT-SFT
   cvis-tmu/Video-R1-7B
 )
